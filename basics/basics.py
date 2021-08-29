24d26c97881c5feffec51189d4de1c1f1ceff8c6
Danilo Silva <danlsil@amazon.com>
2021-08-29 19:33:43 +0100

    incuding doc

---
+++ basics/basics.py
@@ -0,0 +1,73 @@
+# Math operation in pyton
+
+# Soma
+1 + 1
+
+# Multiplicacao
+1 * 3
+
+# Divisao
+1 / 2
+
+# Potenciacao
+3 ** 2
+
+# Funcao type - sample 1
+# Retorna o tipo do objeto colocado como argumento
+type(1)
+
+# Funcao type - sample 2
+
+# Mod %
+5 % 2
+
+# Expressoes com operacoes basicas
+(3 / 2) * (4 +1)
+
+# Definicao de variaveis
+# Obs. Variavies nao iniciam com numeros ou caracteres especais
+# Sao case sensitive
+#Exemplo 1
+
+string = 'nome da variavel'
+print(string)
+type(string)
+
+
+#Imprimindo
+
+var = "Danilo"
+idade = 34
+print('nome_da_variavel: Meu nome é {one} e tenho {two} anos de idade'.format(one=var, two=idade))
+
+# Listas
+# Exemplo
+
+lista = [1, 2, 3]
+type(lista)
+
+# Listas permitem todo tipo de valor, inclusive outra lista
+
+lista2 = [1, 2 ,3 , ['a', 'b', 'c']]
+print(lista2)
+lista2[3]
+
+#Imprimindo lista
+
+# exemplo com range (inicio : fim)
+
+lista = [1, 2 ,3 , ['a', 'b', 'c']]
+
+#imprimindo "a partir de..."
+lista[2:] # irah imprimir a partir do indice 2
+
+#imprimindo "ateh..."
+lista[:2] # irah imprimir ateh o indice 2
+
+#imprimindo "a partir de... ateh ..."
+lista[1:3] 
+
+
+
+
+
