a445859ebc1eaf4cb70af5a9617d340a8eab882c
Danilo Silva <danlsil@amazon.com>
2021-09-04 09:47:02 +0100

    feat/Add lambda function to start instances

---
+++ AWS/LambdaFunctions/startingEC2.py
@@ -0,0 +1,9 @@
+import boto3
+
+region = 'us-east-1'
+instances = ['i-02fdea0754c02738e']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, conext):
+    ec2.start_instances(InstanceIds=instances)
+    print('started your inastances: '+ str(instances))
